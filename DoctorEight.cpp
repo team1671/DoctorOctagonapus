a640db116a33e09b2dc34751cb5d8893a1086901
Gage <buchananrobotics@gmail.com>
2012-02-01 21:11:17 -0800

    Update DoctorEight.cpp

--- DoctorEight.cpp
+++ DoctorEight.cpp
@@ -1,47 +1,45 @@
 /*
  *	Gage Ervin and Emmanuel Lopez		Team 1671 
- *	what happened to the other 7?
- *	historical references: Vriska and Doctaoctagonapus
  */
 
 
 /*
  * COPILOT CONTROL
+ *
  * 1 to turn (hold)
+ *
  * 2 to shoot
+ *
  * trigers will move arm
+ *
  * intake is right thumbstick
- * WHEN SHOOTING ---- IMPORTANT!!!  UNLESS AT LEAST ONE TARGET IS IN SITE OR WE GET A COMPASS LATER DO NOT SHOOT
  * 
- * PILOT
- * car drive, but turns full forward/back at extreme
- * 1 will switch drive
-*/
- 
+ * WHEN SHOOTING ---- IMPORTANT!!!
+ * UNLESS AT LEAST ONE TARGET IS IN SITE OR WE GET A COMPASS LATER DO NOT SHOOT
+ */
 
 /*
- * determine lighting values
+ * PILOT
  *
- * launch enc for speed ratio
+ * car drive, but turns full forward/back at extreme
  *
- * make warning if out of range
- * 
+ * 1 will switch drive
 */
 
+//to do:	launch code;	
 
 #include <iostream>
-//for console IO
 #include "WPILib.h"
-
-//duh
 #include "nivision.h"
 //for cams
 #include "cmath"
+//for distance algorithm
 
 #define IO (DriverStation::GetInstance()->GetEnhancedIO())
-//IO from driver station
+//IO for remotes
 
-#define CAMERAHEIGHT 80//inches
+#define CAMERAHEIGHT 80
+//inches -- correct later
 
 AxisCamera &camera = AxisCamera::GetInstance();
 //output from cameras to driverstation (so we can see it)
@@ -53,27 +51,42 @@ class DoctaEight : public SimpleRobot
 	BinaryImage* binImg;
 	//make image	
 	
-	Encoder LTopEnc, LBotEnc;//launch system encoders
+	Encoder LTopEnc, LBotEnc;
+	//launch system encoders
+	
 	Joystick pilot, copilot;
 	
-	CANJaguar lefty, righty, leftyB, rightyB, intake, LTop, LBot, arm;//left and right motors, recieve ball, lift ball to launching system, launch system, platform arm
-	signed char negate, choiceTarget, distanceTarget, itt;//negate for turning drive
-	double firstTarget, secondTarget, thirdTarget, decrement;//calculated distance to target, to slow motors as aiming
+	CANJaguar lefty, righty, leftyB, rightyB, intake, LTop, LBot, arm;
+	//left and right motors, recieve ball, lift ball to launching system, launch system, platform arm
+	
+	signed char negate, choiceTarget, distanceTarget, itt;
+	//negate for turning drive, choice target target selected, distance target for getting distance, itt for itterations
+	
+	double firstTarget, secondTarget, thirdTarget, decrement;
+	//targets (fourth is not one two or three XD), decriment to slow motors as aiming
 
-	bool limitedDistance;//switch between primary and secondary distance tracking based on number of targets
+	bool limitedDistance;
+	//switch between primary and secondary distance tracking based on number of targets
 	
 public:
 	DoctaEight(void);
-	void targetSelect(void);//selects target	
+	//autonomous and driver control
+	void targetSelect(void);
+	//selects target
 	void aim(void);
-	double fOfX(double x);
+	//turns to aim
+	double fOfX(double x);//part of algorithm
 	double getDistance();
+	//finds distance using immage
 	void shoot(void);
+	//uses formula and distance to set jag percentages
 	void drive(void);
-	void RainbowDash(void)//pony works like c code braces, like this rainbow.Add<Typegoeshere>(variable)
+	//the drive system
+	void RainbowDash(void)
+	//pony works like c code braces, like this rainbow.Add<Typegoeshere>(variable)
 	{
 		Dashboard &rainbow = DriverStation::GetInstance()->GetHighPriorityDashboardPacker();
-		rainbow.AddCluster();						//displays the target nubmers
+		rainbow.AddCluster();//displays the target nubmers
 			rainbow.AddDouble(firstTarget);
 			rainbow.AddDouble(secondTarget);
 			rainbow.AddDouble(thirdTarget);
@@ -88,20 +101,24 @@ public:
 	void Autonomous(void)
 	{
 		GetWatchdog().Kill();
-		while (IsAutonomous())
-		{
-			aim();
-			shoot();
-			//aim and shoot
-		}
+		cout << "Hello???  Can anyone hear me?" << endl;
+		//self aware joke
+		aim();
+		cout << "Hello???  Can anyone hear me?" << endl;
+		shoot();
+		cout << "Hello???  Can anyone hear me?" << endl;
 	}
 	
 	void OperatorControl(void)
 	{
-		GetWatchdog().Kill();
+		GetWatchdog().Kill();			//PK puppy
+		
+		cout << "Operator" << endl;
+		
 		while (IsOperatorControl())
 		{
-			//RainbowDash();
+			
+			RainbowDash();//output to dashboard
 			
 			if (copilot.GetTop())
 				arm.Set(-1);
@@ -115,14 +132,19 @@ public:
 			if (copilot.GetRawButton(1))
 			{
 				aim();
-				//aim
+				cout << "Operator" << endl;
 			}
 			else
 				decrement = 1;
 			//if not aiming and shooting, return motor power
 			
+			
 			if (copilot.GetRawButton(2))
+			{
 				shoot();
+				cout << "Operator" << endl;
+			}
+			
 			
 			intake.Set(copilot.GetTwist());
 			//take the balls
@@ -140,14 +162,12 @@ public:
 	}
 };
 
-//called by driverstation
-
 #include "BuildAndConstruct.h"
+
+#include "SpideySense.h"
 
 #include "VisionEightFold.h"
 
 #include "Tardis.h"
-
-#include "SpideySense.h"
 
 START_ROBOT_CLASS(DoctaEight);

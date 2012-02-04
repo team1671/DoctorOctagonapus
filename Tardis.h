f2f15aa0409e03dd02e3b2970ccfd09773415fe5
Buchanan Robotics <buchananrobotics@gmail.com>
2012-02-04 01:33:23 -0800

    Update Tardis.h

--- Tardis.h
+++ Tardis.h
@@ -1,40 +1,46 @@
-
 void DoctaEight::drive(void)
 {
-	GetWatchdog().Kill();				
-	if (pilot.GetY() > 0 && pilot.GetX() >= 0)//forward right
+	GetWatchdog().Kill();
+	
+	if (pilot.GetDirectionDegrees()/180 <1)
+		xCircle = pilot.GetDirectionDegrees()/180;
+	else if (pilot.GetDirectionDegrees()/180 >1)
+		xCircle = -1+(pilot.GetDirectionDegrees()-180)/180;
+	else {xCircle = 0;}
+	
+	if (pilot.GetTwist() > 0 && xCircle > 0)//forward right
 	{
-		lefty.Set(pilot.GetY() * negate * decrement);//left motors full
-		leftyB.Set(pilot.GetY() * negate * decrement);//left motors full
-		righty.Set(( pilot.GetY() - pilot.GetX() * 2 ) * negate * decrement);//right motors full dec by twiceX abs X
-		rightyB.Set(( pilot.GetY() - pilot.GetX() * 2 ) * negate * decrement);
+		lefty.Set(pilot.GetTwist() * negate  );//left motors full
+		leftyB.Set(pilot.GetTwist() * negate  );//left motors full
+		righty.Set(( pilot.GetTwist() - xCircle * 4 ) * negate  );//right motors full dec by twiceX abs X
+		rightyB.Set(( pilot.GetTwist() - xCircle * 4 ) * negate  );
 	}//(so up to x = 0 right rev and when y negative, backward curve)
-	else if (pilot.GetY() < 0 && pilot.GetX() > 0)//backward left
+	else if (pilot.GetTwist() < 0 && xCircle > 0)//backward left (b/c car drive)
 	{
-		righty.Set(pilot.GetY() * negate * -1 * decrement);
-		rightyB.Set(pilot.GetY() * negate * -1 * decrement);
-		lefty.Set((pilot.GetY() - pilot.GetX() * 2) * negate * decrement);
-		leftyB.Set((pilot.GetY() - pilot.GetX() * 2) * negate * decrement);
+		righty.Set(pilot.GetTwist() * negate);
+		rightyB.Set(pilot.GetTwist() * negate);
+		lefty.Set((pilot.GetTwist() + (xCircle-1) * 4) * negate  );
+		leftyB.Set((pilot.GetTwist() + (xCircle-1) * 4) * negate  );
 	}
-	else if (pilot.GetY() > 0 && pilot.GetX() <= 0)//forward left
+	else if (pilot.GetTwist() > 0 && xCircle < 0)//forward left
 	{
-		righty.Set(pilot.GetY() * negate * decrement);
-		rightyB.Set(pilot.GetY() * negate * decrement);
-		lefty.Set((pilot.GetX() * 2 + pilot.GetY()) * negate * decrement);
-		leftyB.Set((pilot.GetX() * 2 + pilot.GetY()) * negate * decrement);
+		righty.Set(pilot.GetTwist() * negate  );
+		rightyB.Set(pilot.GetTwist() * negate  );
+		lefty.Set((pilot.GetTwist() - (1+xCircle)*4) * negate  );
+		leftyB.Set((pilot.GetTwist() - (1+xCircle)*4) * negate  );
 	}
-	else if (pilot.GetY() < 0 && pilot.GetX() < 0)//back right
+	else if (pilot.GetTwist() < 0 && xCircle < 0)//back right
 	{
-		lefty.Set(pilot.GetY() * negate * -1 * decrement);//left full back
-		leftyB.Set(pilot.GetY() * negate * -1 * decrement);//left full back
-		righty.Set(( pilot.GetY() + pilot.GetX() * 2 ) * negate * decrement);//right morots full dec by twice abs X
-		rightyB.Set(( pilot.GetY() + pilot.GetX() * 2 ) * negate * decrement);
+		lefty.Set(-pilot.GetTwist() * negate);//left full back
+		leftyB.Set(-pilot.GetTwist() * negate);//left full back
+		righty.Set(( pilot.GetTwist() + xCircle * 4 ) * negate  );//right morots full dec by twice abs X
+		rightyB.Set(( pilot.GetTwist() + xCircle * 4 ) * negate  );
 	}
 	else
 	{
-		righty.Set(pilot.GetY() *negate * decrement);
-		rightyB.Set(pilot.GetY() *negate * decrement);
-		lefty.Set(pilot.GetY() *negate * decrement);
-		leftyB.Set(pilot.GetY() *negate * decrement);
+		righty.Set(pilot.GetTwist() *negate  );
+		rightyB.Set(pilot.GetTwist() *negate  );
+		lefty.Set(pilot.GetTwist() *negate  );
+		leftyB.Set(pilot.GetTwist() *negate  );
 	}
 }

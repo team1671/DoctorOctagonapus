a5541981ce5482e6082f9e62563de193e1caab61
Buchanan Robotics <buchananrobotics@gmail.com>
2012-02-08 16:04:29 -0800

    Update DoctorEight.cpp

--- DoctorEight.cpp
+++ DoctorEight.cpp
@@ -1,23 +1,19 @@
+
+
+
+
+//	see	doc8.h	for	todo
+
+
+
+
+
+
 #include "DoctorEight.h"
 
 DoctaEight::DoctaEight(void):
 //constructs
-pilot(1),
-copilot(2),
-//controller(USB port)
-
-lefty(4),
-leftyB(5),
-righty(2),
-rightyB(3),
-
-arm(9),
-intake(8),
-LTop(6),
-LBot(7),
-//jag(CANjag number)
-LTopEnc(1,2),
-LBotEnc(3,4)
+copilot(2)
 //encoders(AChannel, BChannel)
 {
 	GetWatchdog().Kill();
@@ -38,88 +34,6 @@ LBotEnc(3,4)
 
 
 	limitedDistance = 0;
-	cycle = 0;
-	negate=1;
-	drive=1;
-	
-	lefty.ChangeControlMode(CANJaguar::kPercentVbus);
-	righty.ChangeControlMode(CANJaguar::kPercentVbus);
-	leftyB.ChangeControlMode(CANJaguar::kPercentVbus);
-	rightyB.ChangeControlMode(CANJaguar::kPercentVbus);
-	intake.ChangeControlMode(CANJaguar::kPercentVbus);
-	LTop.ChangeControlMode(CANJaguar::kPercentVbus);
-	LBot.ChangeControlMode(CANJaguar::kPercentVbus);
-	//CANJags currently % (-1 to 1)
-	
-	LTopEnc.Reset();
-	LBotEnc.Reset();
-	//reset encoders
-	LTopEnc.Start();
-	LBotEnc.Start();
-	//start encoders
-}
-
-void DoctaEight::tardis(void)
-{
-	if (pilot.GetRawButton(1) && cycle == 0)
-	{
-		negate *= -1;
-		cycle = 1;
-	}
-	if (pilot.GetRawButton(4) && cycle == 0)
-	{
-		drive *= -1;
-		cycle = 1;
-	}
-	else if (!pilot.GetRawButton(1))
-		cycle = 0;
-	//to reverse drive			
-	
-	if (drive == 1)
-	{
-		if (pilot.GetY() > 0 && pilot.GetZ() >= 0)
-		{
-			righty.Set(negate*(pilot.GetY() - pilot.GetZ()*2*pilot.GetY()));
-			rightyB.Set(negate*(pilot.GetY() - pilot.GetZ()*2*pilot.GetY()));
-			lefty.Set(negate*pilot.GetY());
-			leftyB.Set(negate*pilot.GetY());
-		}
-		else if (pilot.GetY() > 0 && pilot.GetZ() <= 0)
-		{
-			righty.Set(negate*pilot.GetY());
-			rightyB.Set(negate*pilot.GetY());
-			lefty.Set(negate*(pilot.GetY() + pilot.GetZ()*2*pilot.GetY()));
-			leftyB.Set(negate*(pilot.GetY() + pilot.GetZ()*2*pilot.GetY()));
-		}		
-		else if (pilot.GetY() < 0 && pilot.GetZ() >= 0)
-		{
-			righty.Set(negate*(pilot.GetY() - pilot.GetZ()*2*pilot.GetY()));
-			rightyB.Set(negate*(pilot.GetY() - pilot.GetZ()*2*pilot.GetY()));
-			lefty.Set(negate*pilot.GetY());
-			leftyB.Set(negate*pilot.GetY());
-		}
-		else if (pilot.GetY() < 0 && pilot.GetZ() <= 0)
-		{
-			righty.Set(negate*pilot.GetY());
-			rightyB.Set(negate*pilot.GetY());
-			lefty.Set(negate*(pilot.GetY() + pilot.GetZ()*2*pilot.GetY()));
-			leftyB.Set(negate*(pilot.GetY() + pilot.GetZ()*2*pilot.GetY()));
-		}
-		else
-		{
-			righty.Set(0);
-			rightyB.Set(0);
-			lefty.Set(0);
-			leftyB.Set(0);
-		}
-	}
-	else if (drive == -1)
-	{
-		righty.Set(negate*pilot.GetTwist());
-		rightyB.Set(negate*pilot.GetTwist());
-		lefty.Set(negate*pilot.GetZ());
-		leftyB.Set(negate*pilot.GetZ());
-	}
 }
 
 void DoctaEight::Autonomous(void)
@@ -128,30 +42,25 @@ void DoctaEight::Autonomous(void)
 	driverOut->PrintfLine(DriverStationLCD::kUser_Line1, "auto start");
 	driverOut->UpdateLCD();
 	aim();
-	driverOut->PrintfLine(DriverStationLCD::kUser_Line1, "auto mid");
+	driverOut->PrintfLine(DriverStationLCD::kUser_Line2, "auto mid");
 	driverOut->UpdateLCD();
 	shoot();
-	driverOut->PrintfLine(DriverStationLCD::kUser_Line1, "auto end");
+	driverOut->PrintfLine(DriverStationLCD::kUser_Line3, "auto end");
 	driverOut->UpdateLCD();
 }
 
 void DoctaEight::OperatorControl(void)
 {
 	GetWatchdog().Kill();
+	driverOut->Clear();
+	driverOut->UpdateLCD();
 	
 	while (IsOperatorControl())
 	{
 
-	driverOut->PrintfLine(DriverStationLCD::kUser_Line1, "operator");
-	driverOut->UpdateLCD();
-		
-		if (copilot.GetTop())
-			arm.Set(-1);
-		else if (copilot.GetTrigger())
-			arm.Set(1);
-		else
-			arm.Set(0);
-		//move arm
+		driverOut->Clear();
+		driverOut->PrintfLine(DriverStationLCD::kUser_Line1, "operator");
+		driverOut->UpdateLCD();
 		
 		above:
 		//this is because targets may momentarily not be tracked in which case I want to re-execute
@@ -170,15 +79,7 @@ void DoctaEight::OperatorControl(void)
 		
 		if (copilot.GetRawButton(2) or copilot.GetRawButton(1))
 			goto above;
-		
-		intake.Set(copilot.GetTwist());
-		//take the balls
-
-		tardis();		
 	}
-	LTopEnc.Stop();
-	LBotEnc.Stop();
-	//stops encoders
 }
 
 START_ROBOT_CLASS(DoctaEight);

6598f8eb24c252cbccd599fdf37ae50b66c2c1a0
Eman96 <Iameman9669@gmail.com>
2012-02-11 17:30:54 +0800

    All the variables for the camera have been grouped together into a struct. Also Gage you did use a recursive loop. values has target select and vice versa. (I have no idea how to fix that.)

--- DoctorEight.cpp
+++ DoctorEight.cpp
@@ -40,7 +40,21 @@ class DoctaEight : public SimpleRobot
 
 	bool limitedDistance, cycle, flag, aimin, shootin;
 	//switch between primary and secondary distance tracking based on number of targets
-	
+	struct CamDataStruct//group for camera data use UpdateCamData before using these
+	{
+		//char choiceTarget;
+		int numTargets;
+		double toCenter;
+		double centerY;
+		double centerX;
+		double firstY;
+		double secondY;
+		double thirdY;
+		double fourthY;
+		double area;
+		double height;
+		double distanceYnorm;
+	}CamData;
 public:
 	DoctaEight::DoctaEight(void):
 	copilot(2)
@@ -50,7 +64,6 @@ public:
 		GetWatchdog().Kill();
 		
 		driverOut->UpdateLCD();
-		
 		//camera.WriteMaxFPS(30);
 		//camera.WriteBrightness(30);
 		//camera.WriteWhiteBalance(AxisCamera::kWhiteBalance_Hold);
@@ -79,48 +92,43 @@ public:
 	void targetSelect(int &choiceTarget)//pick the target to shoot at (highest visible) then the one to distance with
 	{
 		GetWatchdog().Kill();
-//		numTargets, toCenter, centerY, centerX, firstY, secondY, thirdY, fourthY, area, height, distanceYnorm
-
-
-		int numTargets;
-		double toCenter, centerY, centerX, firstY, secondY, thirdY, fourthY, area, height, distanceYnorm;
-		values(numTargets, toCenter, centerY, centerX, firstY, secondY, thirdY, fourthY, area, height, distanceYnorm);
+		UpdateCamData();
 				
-		if (3 == numTargets or 4 == numTargets)//if 3 or 4 targets visible
+		if (3 == CamData.numTargets or 4 == CamData.numTargets)//if 3 or 4 targets visible
 		{
 			//choose target to aim/shoot at -> highest target
-			if (firstY > secondY && firstY > thirdY)
+			if (CamData.firstY > CamData.secondY && CamData.firstY > CamData.thirdY)
 				choiceTarget = 1;
-			else if (secondY > thirdY && secondY > firstY)
+			else if (CamData.secondY > CamData.thirdY && CamData.secondY > CamData.firstY)
 				choiceTarget = 2;
-			else if (thirdY > secondY && thirdY > firstY)
+			else if (CamData.thirdY > CamData.secondY && CamData.thirdY > CamData.firstY)
 				choiceTarget = 3;
 			else {choiceTarget = 4;}
 
 			//find lowest target for distance tracking purposes
-			if (firstY < secondY && firstY < thirdY)
+			if (CamData.firstY < CamData.secondY && CamData.firstY < CamData.thirdY)
 				distanceTarget = 1;
-			else if (secondY < thirdY && secondY < firstY)
+			else if (CamData.secondY < CamData.thirdY && CamData.secondY < CamData.firstY)
 				distanceTarget = 2;
-			else if (thirdY < secondY && thirdY < firstY)
+			else if (CamData.thirdY < CamData.secondY && CamData.thirdY < CamData.firstY)
 				distanceTarget = 3;
 			else 
 				distanceTarget = 4;
 
 			limitedDistance = 0;//switch between high and low accuracy distance tracking (high requires 3 or 4 particles)
 		}
-		else if (2 == numTargets)//if 2 targets visible
+		else if (2 == CamData.numTargets)//if 2 targets visible
 		{
 
 			//select target to aim/shoot at
-			if (firstY > secondY)
+			if (CamData.firstY > CamData.secondY)
 				choiceTarget = 1;
 			else
 				choiceTarget = 2;
 
 			limitedDistance = 1;//use single target target tracking
 		}
-		else if (1 == numTargets)
+		else if (1 == CamData.numTargets)
 		{
 			//aim/shoot at only target
 			choiceTarget = 1;
@@ -141,28 +149,27 @@ public:
 	{
 		GetWatchdog().Kill();
 		
+		UpdateCamData();
+		
 		int choiceTarget;
 		targetSelect(choiceTarget);
 		
-		int numTargets;
-		double toCenter, centerY, centerX, firstY, secondY, thirdY, fourthY, area, height, distanceYnorm;
-		values(numTargets, toCenter, centerY, centerX, firstY, secondY, thirdY, fourthY, area, height, distanceYnorm);
-				
-		if (choiceTarget !=-1 && numTargets < 5)//if there is a target
+		
+		if (choiceTarget !=-1 && CamData.numTargets < 5)//if there is a target
 		{
 			if (limitedDistance == 1)//if must use less accurate tracking
 			{
 				aproximation = 9//half height of target in inches over target to get adjacent
 								/tan(//tan of this to get opposite over adjacent
 										angle*
-											((area/24)//to get height in pixels
-												/height)//above divided to get ratio of size
+											((CamData.area/24)//to get height in pixels
+												/CamData.height)//above divided to get ratio of size
 													/2);//to get half of angle and therefore right triangle
 			}
 			else//better tracking
 			{
 				
-				double theta=(centerY-distanceYnorm)/240*angle;//the distance is turned into an angle (refer to fofx(x))
+				double theta=(CamData.centerY-CamData.distanceYnorm)/240*angle;//the distance is turned into an angle (refer to fofx(x))
 				//107.5 is top target height 31.5 is bottom target height
 				
 				double dotbinary=54;
@@ -182,7 +189,8 @@ public:
 			aproximation= -1;
 	}
 	
-	void values (int &numTargets, double &toCenter, double &centerY, double &centerX, double &firstY, double &secondY, double &thirdY, double &fourthY, double &area, double &height, double &distanceYnorm)
+	//numTargets, toCenter, centerY, centerX, firstY, secondY, thirdY, fourthY, area, height, distanceYnorm
+	void UpdateCamData()
 	{
 		//gives cam info
 		int choiceTarget;
@@ -199,24 +207,24 @@ public:
 		particles = filteredImage->GetOrderedParticleAnalysisReports();  // get the results		
 
 			ParticleAnalysisReport& par = (*particles)[choiceTarget];//get values for centering from selected target
-			numTargets = particles->size();
-			centerX = par.center_mass_x_normalized;
-			centerY = par.center_mass_y_normalized;
+			CamData.numTargets = particles->size();
+			CamData.centerX = par.center_mass_x_normalized;
+			CamData.centerY = par.center_mass_y_normalized;
 			
 			par = (*particles)[1];
-			firstY = par.center_mass_y;
+			CamData.firstY = par.center_mass_y;
 			par = (*particles)[2];
-			secondY = par.center_mass_y;
+			CamData.secondY = par.center_mass_y;
 			par = (*particles)[3];
-			thirdY = par.center_mass_y;
+			CamData.thirdY = par.center_mass_y;
 			par = (*particles)[4];
-			fourthY = par.center_mass_y;
+			CamData.fourthY = par.center_mass_y;
 			
 			par = (*particles)[distanceTarget];
-			area = par.particleArea;
-			height = par.imageHeight;
+			CamData.area = par.particleArea;
+			CamData.height = par.imageHeight;
 			
-			distanceYnorm = par.center_mass_y_normalized;
+			CamData.distanceYnorm = par.center_mass_y_normalized;
 		
 
 		delete filteredImage;
@@ -224,12 +232,12 @@ public:
 		delete bigObjectsImage;
 		delete thresholdImage;
 	}
-	
+		
 	void output (void)
 	{
-		int numTargets, choiceTarget;
-		double toCenter, centerY, centerX, firstY, secondY, thirdY, fourthY, area, height, distanceYnorm, aproximation;
-		values(numTargets, toCenter, centerY, centerX, firstY, secondY, thirdY, fourthY, area, height, distanceYnorm);
+		UpdateCamData();
+		int choiceTarget;
+		double aproximation;
 		//GET INFO FROM values()
 		if (IsAutonomous())
 			driverOut->Printf(DriverStationLCD::kUser_Line1, 1, "Auto");
@@ -243,9 +251,9 @@ public:
 
 		driverOut->Printf(DriverStationLCD::kUser_Line3, 3, "Distance: %d", aproximation);
 		
-		driverOut->Printf(DriverStationLCD::kUser_Line3, 3, "Number of targets: %n", numTargets);
+		driverOut->Printf(DriverStationLCD::kUser_Line3, 3, "Number of targets: %n", CamData.numTargets);
 		driverOut->Printf(DriverStationLCD::kUser_Line4, 4, "Target selected: %n", choiceTarget);
-		driverOut->Printf(DriverStationLCD::kUser_Line5, 5, "Double to 0: %d", centerX);
+		driverOut->Printf(DriverStationLCD::kUser_Line5, 5, "Double to 0: %d", CamData.centerX);
 		
 		if (choiceTarget == -1)
 			driverOut->Printf(DriverStationLCD::kUser_Line6, 6, "no target");

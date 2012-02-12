cd8cf3a40a795a5c14f05ab35aabc173819906be
Buchanan Robotics <buchananrobotics@gmail.com>
2012-02-11 16:33:19 -0800

    Update DoctorEight.cpp

--- DoctorEight.cpp
+++ DoctorEight.cpp
@@ -1,14 +1,3 @@
-
-
-
-
-//CAM ONLY VERSION
-
-
-
-
-
-
 #include "WPILib.h"
 #include "Vision/RGBImage.h"
 #include "Vision/BinaryImage.h"
@@ -18,7 +7,13 @@
 #define ANGLEOFLAUNCH 45
 #define angle 54
 #define pi 3.14159265358979323846264
-static AxisCamera &camera = AxisCamera::GetInstance("10.16.71.11");
+#define RL 245
+#define RH 255
+#define GL 245
+#define GH 255
+#define BL 240
+#define BH 255
+static AxisCamera &eyeOfSauron = AxisCamera::GetInstance("10.16.71.11");
 
 DriverStationLCD* driverOut = DriverStationLCD::GetInstance();
 
@@ -32,7 +27,7 @@ class DoctaEight : public SimpleRobot
 	BinaryImage *filteredImage;
 	vector<ParticleAnalysisReport> *particles;
 
-	signed char distanceTarget;
+	signed char distanceTarget, choiceTarget;
 	//negate for turning drive, choice target target selected, distance target for getting distance, itt for itterations
 	
 	double firstTarget, secondTarget, thirdTarget;
@@ -42,18 +37,17 @@ class DoctaEight : public SimpleRobot
 	//switch between primary and secondary distance tracking based on number of targets
 	struct CamDataStruct//group for camera data use UpdateCamData before using these
 	{
-		//char choiceTarget;
 		int numTargets;
-		double toCenter;
-		double centerY;
-		double centerX;
-		double firstY;
-		double secondY;
-		double thirdY;
-		double fourthY;
-		double area;
-		double height;
-		double distanceYnorm;
+		double toCenter, 
+			centerY, 
+				centerX, 
+					firstY, 
+						secondY, 
+							thirdY, 
+								fourthY, 
+									area, 
+										height, 
+											distanceYnorm;
 	}CamData;
 public:
 	DoctaEight::DoctaEight(void):
@@ -64,12 +58,12 @@ public:
 		GetWatchdog().Kill();
 		
 		driverOut->UpdateLCD();
-		//camera.WriteMaxFPS(30);
-		//camera.WriteBrightness(30);
-		//camera.WriteWhiteBalance(AxisCamera::kWhiteBalance_Hold);
-		//camera.WriteResolution(AxisCamera::kResolution_640x360);
-		//camera.WriteColorLevel(100);
-		//camera.WriteCompression(30);
+		//eyeOfSauron.WriteMaxFPS(30);
+		//eyeOfSauron.WriteBrightness(30);
+		//eyeOfSauron.WriteWhiteBalance(AxisCamera::kWhiteBalance_Hold);
+		//eyeOfSauron.WriteResolution(AxisCamera::kResolution_640x360);
+		//eyeOfSauron.WriteColorLevel(100);
+		//eyeOfSauron.WriteCompression(30);
 		//lower easier on CRIO and harder on cam
 		limitedDistance = 0;
 	}
@@ -86,13 +80,15 @@ public:
 	void DoctaEight::OperatorControl(void)
 	{
 		GetWatchdog().Kill();
-		//output();
+		while (IsOperatorControl() && IsEnabled())
+		{
+			output();
+		}
 	}
 	
-	void targetSelect(int &choiceTarget)//pick the target to shoot at (highest visible) then the one to distance with
+	void targetSelect(void)//pick the target to shoot at (highest visible) then the one to distance with
 	{
 		GetWatchdog().Kill();
-		UpdateCamData();
 				
 		if (3 == CamData.numTargets or 4 == CamData.numTargets)//if 3 or 4 targets visible
 		{
@@ -137,22 +133,19 @@ public:
 		else
 			choiceTarget = -1;
 	}
-
+	
 	double fOfX(double x)//part of accurate distance finding in getDistance()
 	{
 		GetWatchdog().Kill();
 		return 	(atan((107.5-CAMERAHEIGHT)/(x)) //angle from top triangle (triangle formed by camera target and line at camera's height)
 				+atan((CAMERAHEIGHT-31.5)/(x))); //angle from bottom triangle
-	}
+	}	
 	
 	void getDistance(double &aproximation)//find distance
 	{
 		GetWatchdog().Kill();
 		
-		UpdateCamData();
-		
-		int choiceTarget;
-		targetSelect(choiceTarget);
+		targetSelect();
 		
 		
 		if (choiceTarget !=-1 && CamData.numTargets < 5)//if there is a target
@@ -181,8 +174,7 @@ public:
 				if(fOfX(aproximation+dotbinary)>theta) //if the value to be added overshoots it does not add
 					aproximation+=dotbinary;
 				}
-			}
-			
+			}		
 			
 		}
 		if (choiceTarget == -1 or choiceTarget > 4)//if there is no target return -1 to indicate target not found -- must be interpeted in 'shoot'
@@ -192,20 +184,21 @@ public:
 	//numTargets, toCenter, centerY, centerX, firstY, secondY, thirdY, fourthY, area, height, distanceYnorm
 	void UpdateCamData()
 	{
-		//gives cam info
-		int choiceTarget;
-		targetSelect(choiceTarget);
-		Threshold threshold(60, 140, 20, 110, 0, 60);
+		Threshold thresholdRGB(RL,RH,GL,GH,BL,BH);
 		ParticleFilterCriteria2 criteria[] = {{IMAQ_MT_BOUNDING_RECT_WIDTH, 30, 400, false, false},
 											{IMAQ_MT_BOUNDING_RECT_HEIGHT, 40, 400, false, false}};
+		//false Set this element to TRUE to take calibrated measurements.
+		//fales Set this element to TRUE to indicate that a match occurs when the measurement is outside the criteria range
+		
 		ColorImage image(IMAQ_IMAGE_RGB);//make image
-		camera.GetImage(&image);//store camera feed to image
-		thresholdImage = image.ThresholdRGB(threshold);//get ready to abliterate insignifigant objects
+		eyeOfSauron.GetImage(&image);//store camera feed to image
+		thresholdImage = image.ThresholdRGB(thresholdRGB);//abliteration prep
 		bigObjectsImage = thresholdImage->RemoveSmallObjects(false, 2);  // remove small objects (noise)
 		convexHullImage = bigObjectsImage->ConvexHull(false);  // fill in partial and full rectangles
 		filteredImage = convexHullImage->ParticleFilter(criteria, 2);  // find the rectangles
 		particles = filteredImage->GetOrderedParticleAnalysisReports();  // get the results		
-
+	
+			targetSelect();
 			ParticleAnalysisReport& par = (*particles)[choiceTarget];//get values for centering from selected target
 			CamData.numTargets = particles->size();
 			CamData.centerX = par.center_mass_x_normalized;
@@ -225,12 +218,59 @@ public:
 			CamData.height = par.imageHeight;
 			
 			CamData.distanceYnorm = par.center_mass_y_normalized;
-		
-
+		delete &image;
 		delete filteredImage;
 		delete convexHullImage;
 		delete bigObjectsImage;
 		delete thresholdImage;
+		/*for (char i = 0; i < 3; i++)//IF THIS VERSION USED, NEED ARRAY FOR CHOICE TARGET
+		{//DISTANCE APROXIMATION, AND EVERY VARRIABLE MADE IN THIS
+			int rl,rh,gl,gh,bl,bh, RGB [6] = {rl = RL,rh = RH,gl = GL,gh = GH,bl = BL,bh = BH};
+			for (char e=2; e<6; e=e+2)
+			{
+				RGB[(i+e)%6] = 0;
+				RGB[(i+e+1)%6] = 255;
+			}
+			Threshold thresholdRGB(RGB[0],RGB[1],RGB[2],RGB[3],RGB[4],RGB[5]);
+			ParticleFilterCriteria2 criteria[] = {{IMAQ_MT_BOUNDING_RECT_WIDTH, 30, 400, false, false},
+												{IMAQ_MT_BOUNDING_RECT_HEIGHT, 40, 400, false, false}};
+			//false Set this element to TRUE to take calibrated measurements.
+			//fales Set this element to TRUE to indicate that a match occurs when the measurement is outside the criteria range
+			
+			ColorImage image(IMAQ_IMAGE_RGB);//make image
+			eyeOfSauron.GetImage(&image);//store camera feed to image
+			thresholdImage = image.ThresholdRGB(thresholdRGB);//abliteration prep
+			bigObjectsImage = thresholdImage->RemoveSmallObjects(false, 2);  // remove small objects (noise)
+			convexHullImage = bigObjectsImage->ConvexHull(false);  // fill in partial and full rectangles
+			filteredImage = convexHullImage->ParticleFilter(criteria, 2);  // find the rectangles
+			particles = filteredImage->GetOrderedParticleAnalysisReports();  // get the results		
+
+				targetSelect();
+				ParticleAnalysisReport& par = (*particles)[choiceTarget];//get values for centering from selected target
+				CamData.numTargets = particles->size();
+				CamData.centerX = par.center_mass_x_normalized;
+				CamData.centerY = par.center_mass_y_normalized;
+				
+				par = (*particles)[1];
+				CamData.firstY = par.center_mass_y;
+				par = (*particles)[2];
+				CamData.secondY = par.center_mass_y;
+				par = (*particles)[3];
+				CamData.thirdY = par.center_mass_y;
+				par = (*particles)[4];
+				CamData.fourthY = par.center_mass_y;
+				
+				par = (*particles)[distanceTarget];
+				CamData.area = par.particleArea;
+				CamData.height = par.imageHeight;
+				
+				CamData.distanceYnorm = par.center_mass_y_normalized;
+			delete &image;
+			delete filteredImage;
+			delete convexHullImage;
+			delete bigObjectsImage;
+			delete thresholdImage;
+		}*/
 	}
 		
 	void output (void)
@@ -246,7 +286,6 @@ public:
 		
 		driverOut->UpdateLCD();
 		
-		targetSelect(choiceTarget);
 		getDistance(aproximation);
 
 		driverOut->Printf(DriverStationLCD::kUser_Line3, 3, "Distance: %d", aproximation);
@@ -261,6 +300,6 @@ public:
 			driverOut->Printf(DriverStationLCD::kUser_Line6, 6, "too many targets");
 		driverOut->UpdateLCD();
 	}
-
+	
 };
 START_ROBOT_CLASS(DoctaEight);

7ed2e709d06b53ff8b153c4ce58131572bfee01a
Buchanan Robotics <buchananrobotics@gmail.com>
2012-02-11 22:35:39 -0800

    Update MyRobot.cpp

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -3,8 +3,12 @@
  * 
  * pilot->y cycles drive: mod arcade/tank/kinect
  * 
- * copilot->hold 3 and press triggers to set a top motor
- * hold 2 and press triggers to set a bot motor
+ * copilot
+ * .3,.4,.5,.6,.7,.8,.9
+ * press LB and a,b,x,y,back,start,left stick button, right stick button to set top
+ * press RB and a,b,x,y,back,start,left stick button, right stick button to set bot
+ * RT will increase overall spd for launch
+ * LT will decrease = =
  * intake is y on left thumb
  * arm is X on right thumb
  * 
@@ -14,29 +18,31 @@
 #include "KinectStick.h"
 
 
-//#define MAX 2750
-//#define KP 0.250
-//#define KI 0.015
-//#define KD 0.020
-//#define ENCCOUNT 360
+#define MAX 2750
+#define KP 0.250
+#define KI 0.015
+#define KD 0.020
+#define ENCCOUNT 360
 
 class DoctaEight : public SimpleRobot
 {	
 	DriverStationLCD* driverOut;
 	//launch system encoders
 	
+	Timer clock;
+	
 	Joystick pilot, copilot;
-	KinectStick *leftArm, *rightArm;
+//	KinectStick *leftArm, *rightArm;
 	
 	CANJaguar lefty, righty, leftyB, rightyB, intake, arm, LTop, LBot;
 	//left and right motors, recieve ball, lift ball to launching system, launch system, platform arm
 
 	signed char negate, choiceTarget, distanceTarget, drive;
 	//negate for turning drive, choice target target selected, distance target for getting distance, itt for itterations
-
-//	Encoder LTopEnc, LBotEnc;
 	
-	bool limitedDistance, cycle, flag;
+	double change;
+	
+	bool limitedDistance, cycle;
 	//switch between primary and secondary distance tracking based on number of targets
 	
 public:
@@ -55,8 +61,6 @@ public:
 	LTop(7),
 	LBot(6)
 //	//jag(CANjag number)
-//	LTopEnc(1,2),
-//	LBotEnc(3,4)
 	//encoders(AChannel, BChannel)
 	{
 		GetWatchdog().Kill();
@@ -65,15 +69,16 @@ public:
 		
 		driverOut  = DriverStationLCD::GetInstance();
 
-		leftArm = new KinectStick(1);
-		rightArm = new KinectStick(2);
+//		leftArm = new KinectStick(1);
+//		rightArm = new KinectStick(2);
 				
 		
+		
 		limitedDistance = 0;
-		flag = 0;
 		cycle = 0;
 		drive=1;
 		negate=1;
+		change= 0;
 
 		lefty.ChangeControlMode(CANJaguar::kPercentVbus);
 		righty.ChangeControlMode(CANJaguar::kPercentVbus);
@@ -82,46 +87,97 @@ public:
 		intake.ChangeControlMode(CANJaguar::kPercentVbus);
 		arm.ChangeControlMode(CANJaguar::kPercentVbus);
 		
-		LTop.ChangeControlMode(CANJaguar::kPercentVbus);
-		LBot.ChangeControlMode(CANJaguar::kPercentVbus);
-//		LBot.ConfigEncoderCodesPerRev(ENCCOUNT);
-//		LTop.ConfigEncoderCodesPerRev(ENCCOUNT);
+		LTop.ChangeControlMode(CANJaguar::kSpeed);
+		LBot.ChangeControlMode(CANJaguar::kSpeed);
+		LBot.ConfigEncoderCodesPerRev(ENCCOUNT);
+		LTop.ConfigEncoderCodesPerRev(ENCCOUNT);
 		//CANJags currently % (-1 to 1)
-//		LBot.SetPID(KP,KI,KD);
-//		LTop.SetPID(KP,KI,KD);
+		LBot.SetPID(KP,KI,KD);
+		LTop.SetPID(KP,KI,KD);
 
-//		LTopEnc.Reset();
-//		LBotEnc.Reset();
-//		//reset encoders
-//		LTopEnc.Start();
-//		LBotEnc.Start();
-//		//start encoders
+		//start encoders
 	}
 	
 
 	void shoot(void)
 	{
-		if (copilot.GetRawButton(3))
-			LTop.Set(copilot.GetZ());
+		if ((int)clock.Get()%10 == 1)
+		{
+			if (copilot.GetZ()>.2)
+				change+=1;
+			if (copilot.GetZ()<.2)
+					change-=1;
+		}
+		
+		if (copilot.GetRawButton(1))
+		{
+			if (copilot.GetRawButton(9))
+				LTop.Set(0);
+			if (copilot.GetRawButton(10))
+				LBot.Set(0);
+		}
 		else if (copilot.GetRawButton(2))
-			LBot.Set(copilot.GetZ());
+		{
+			if (copilot.GetRawButton(9))
+				LTop.Set(.3*MAX+change);
+			if (copilot.GetRawButton(10))
+				LBot.Set(.3*MAX+change);
+		}
+		else if (copilot.GetRawButton(3))
+		{
+			if (copilot.GetRawButton(9))
+				LTop.Set(.4*MAX+change);
+			if (copilot.GetRawButton(10))
+				LBot.Set(.4*MAX+change);
+		}
+		else if (copilot.GetRawButton(4))
+		{
+			if (copilot.GetRawButton(9))
+				LTop.Set(.5*MAX+change);
+			if (copilot.GetRawButton(10))
+				LBot.Set(.5*MAX+change);
+		}
+		else if (copilot.GetRawButton(5))
+		{
+			if (copilot.GetRawButton(9))
+				LTop.Set(.6*MAX+change);
+			if (copilot.GetRawButton(10))
+				LBot.Set(.6*MAX+change);
+		}
+		else if (copilot.GetRawButton(6))
+		{
+			if (copilot.GetRawButton(9))
+				LTop.Set(.7*MAX+change);
+			if (copilot.GetRawButton(10))
+				LBot.Set(.7*MAX+change);
+		}
+		else if (copilot.GetRawButton(7))
+		{
+			if (copilot.GetRawButton(9))
+				LTop.Set(.8*MAX+change);
+			if (copilot.GetRawButton(10))
+				LBot.Set(.8*MAX+change);
+		}
+		else if (copilot.GetRawButton(8))
+		{
+			if (copilot.GetRawButton(9))
+				LTop.Set(.9*MAX+change);
+			if (copilot.GetRawButton(10))
+				LBot.Set(.9*MAX+change);
+		}
+		
 	}
 	
 	void DoctaEight::Autonomous(void)
 	{
 		GetWatchdog().Kill();
 		output();
-//		LTopEnc.Stop();
-//		LBotEnc.Stop();
-//		LTopEnc.Reset();
-//		LBotEnc.Reset();
-		
 	}
 	void DoctaEight::OperatorControl(void)
 	{
 		GetWatchdog().Kill();
-//		LTopEnc.Start();
-//		LBotEnc.Start();
+		clock.Reset();
+		clock.Start();
 
 		while (IsOperatorControl())
 		{
@@ -138,9 +194,8 @@ public:
 			tardis();//robot drive <-below
 			
 		}
-//		LTopEnc.Stop();
-//		LBotEnc.Stop();
-//		//stops encoders
+		clock.Stop();
+		//stops encoders
 	}
 	
 	void output (void)
@@ -170,7 +225,7 @@ public:
 		}
 		else if (pilot.GetRawButton(4) && cycle == 0)
 		{
-			drive = drive%4 + 1;//	GET Y FOR RIGHT THUMB??
+			drive = drive%4 + 2;//	Change to plus 1 for kinect drive be included
 			cycle = 1;
 		}
 		else if (!pilot.GetRawButton(1) && !pilot.GetRawButton(4))
@@ -217,8 +272,7 @@ public:
 		}
 		else if (drive == 2)
 		{
-			GetWatchdog().Kill();
-			leftyrighty (leftArm -> GetY(), rightArm -> GetTwist());
+			//leftyrighty (leftArm -> GetY(), rightArm -> GetTwist());
 		}
 		else if (drive == 3)
 		{

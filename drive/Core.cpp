f8b6db5d94a0a2b301f0deb792fa485bcee58611
Buchanan Robotics <buchananrobotics@gmail.com>
2012-02-18 00:16:37 -0800

    Update drive/Core.cpp

--- drive/Core.cpp
+++ drive/Core.cpp
@@ -1,4 +1,4 @@
-#include"Vrisk.h"
+#include"head.h"
 
 DriverStationLCD* driverOut = DriverStationLCD::GetInstance();
 
@@ -35,35 +35,28 @@ LBot(6)
 	leftyB.ConfigMaxOutputVoltage(kVOLTAGE);
 	righty.ConfigMaxOutputVoltage(kVOLTAGE);
 	rightyB.ConfigMaxOutputVoltage(kVOLTAGE);
-	LTop.ConfigMaxOutputVoltage(kVOLTAGE);
-	LBot.ConfigMaxOutputVoltage(kVOLTAGE);
 	
 	LBot.SetSpeedReference(CANJaguar::kSpeedRef_QuadEncoder);
 	LTop.SetSpeedReference(CANJaguar::kSpeedRef_QuadEncoder);
 	LBot.SetPositionReference(CANJaguar::kPosRef_QuadEncoder);
 	LTop.SetPositionReference(CANJaguar::kPosRef_QuadEncoder);
-	//WHY BOTH???	
 
 	LBot.ConfigEncoderCodesPerRev(kENCCOUNT);
 	LTop.ConfigEncoderCodesPerRev(kENCCOUNT);
-	LTop.SetVoltageRampRate(RAMP);//MAXIMUM
-	LBot.SetVoltageRampRate(RAMP);//V CHANGE
-									//OR I + P
+
 	lefty.SetVoltageRampRate(RAMP);
 	leftyB.SetVoltageRampRate(RAMP);
 	righty.SetVoltageRampRate(RAMP);
 	rightyB.SetVoltageRampRate(RAMP);
 	LTop.EnableControl();
 	LBot.EnableControl();
-	rightyB.EnableControl();
-	righty.EnableControl();
-	leftyB.EnableControl();
-	lefty.EnableControl();
+//	rightyB.EnableControl();
+//	righty.EnableControl();
+//	leftyB.EnableControl();
+//	lefty.EnableControl();
 
-	shootin = 0;
-	negate = 0;
-	drive =1;
-	limitedDistance = 0;
+	negate = 1;
+	drive =3;
 }
 
 
@@ -72,6 +65,7 @@ void DoctaEight::Autonomous(void)
 	Kill;
 	while (IsAutonomous() && IsEnabled())
 	{
+		output();
 	}
 }
 void DoctaEight::OperatorControl(void)
@@ -79,6 +73,7 @@ void DoctaEight::OperatorControl(void)
 	Kill;
 	while (IsOperatorControl() && IsEnabled())
 	{
+		output();
 		tardis();//robot drive <-below
 	}
 }
@@ -86,6 +81,21 @@ void DoctaEight::OperatorControl(void)
 void DoctaEight::output (void)
 {
 	Kill;
+	
+	driverOut->Clear();
+	
+	driverOut->PrintfLine(DriverStationLCD::kUser_Line3, "Top CANJag Temp: %f Celcius",LTop.GetTemperature()*(9/5) + 32);
+	driverOut->PrintfLine(DriverStationLCD::kUser_Line4, "Bot CANJag Temp: %f Celcius",LBot.GetTemperature()*(9/5) + 32);
+	/*
+	 ____.Faults(____::kCurrentFault);
+	 * kTemperatureFault = 2, kBusVoltageFault = 4, kGateDriverFault = 8
+	 *can use above to set fault for some things
+	 * void CANJaguar::ConfigNeutralMode(NeutralMode) 
+	 * fault time can also be set
+	*/
+
+	driverOut->PrintfLine(DriverStationLCD::kUser_Line5, "Top speed: %f",LTop.GetSpeed());
+	driverOut->PrintfLine(DriverStationLCD::kUser_Line6, "Bot speed: %f",LBot.GetSpeed());
 	
 	if (IsAutonomous())
 		driverOut->Printf(DriverStationLCD::kUser_Line1, 1, "Autonomous");

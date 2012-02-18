4bc6d29503f9f606a7463ca019ea1c60da52f8f3
Buchanan Robotics <buchananrobotics@gmail.com>
2012-02-18 00:20:42 -0800

    Update Dr.8/Vrisk.h

--- Dr.8/Vrisk.h
+++ Dr.8/Vrisk.h
@@ -1,44 +1,17 @@
 
 /*
 
-BOTHER SOM1 ABT LED'S
+find auton value for knocking balls down
 
 find trim value for calculated distance to real distance
 dist exp and consider spacial shot exp
-
-convert to real code by testing stuff in vision assistant
-
-manual area over area?
-
-nonglobal
 
 comment
  
 comment here for controls
 
-IN aim() use PID to set cam FPS
-
 set camera to allow anonymous viewing to outline target(available in the web console)
 http://firstforge.wpi.edu/integratio...02&view=markup
-
-
-remember to SetSetpoint(f) for PID's
-PID
-Base should speed up with low I and high D so it never quite goes full in
-op cont
-also a med to low p so there is a curve
-
-PID
-base should slow similarly in auton so it stops only oscilating once or
-not at all
-
-PID
-as turning cam speed should increase in auton while aiming
-
-PID
-shooter motors should have low p for acell curve and an I and D to
-make oscilation small as possible(less stress on motors)
-
  */
 
 #include "WPILib.h"
@@ -53,31 +26,42 @@ make oscilation small as possible(less stress on motors)
 #define PI 3.14159265358979323846264
 
 #define kVOLTAGE 12.0
-#define RAMP .4
+#define RAMP 3
 #define MAX 2750
 #define KP 0.250//proportion for jags
 #define KI 0.015//integral change in proportion
 #define KD 0.020//damper
 #define kENCCOUNT 360
 
-#define HUL 200
-#define HUH 260
-#define SAL 140
-#define SAH 210
-#define LUL 90
-#define LUH 135
-/*
-#define BS 1.0f
-#define CT 1.0f
-#define GA 1.0f
-*/
+#define RL 0
+#define RH 145
+#define GL 0
+#define GH 255
+#define BL 184
+#define BH 255
+
+#define LCRIT 0
+#define HCRIT 90
+
+#define BS 198
+#define CT 69
+#define GA 4
+
+//#define RPMLow 1000 next to
 
 static AxisCamera &eyeOfSauron = AxisCamera::GetInstance("10.16.71.11");
 	
+//#define dsio DriverStation::GetInstance()->GetEnhancedIO()
+//example of use (config in DS)
+//		double ptop = (dsio.GetAnalogInRatio(1) - 0.5) * MAX;
+//		double pbot = (dsio.GetAnalogInRatio(2) - 0.5) * MAX;
+//		LTop.Set(ptop);
+//		LBot.Set(pbot);
+
 class DoctaEight : public SimpleRobot
 {
 	Joystick pilot, copilot;
-	//	KinectStick *leftArm, *rightArm;
+	KinectStick *leftArm, *rightArm;
 	CANJaguar lefty, righty, leftyB, rightyB, intake, arm, LTop, LBot;
 	signed char distanceTarget, choiceTarget, negate, drive;
 	double firstTarget, secondTarget, thirdTarget;

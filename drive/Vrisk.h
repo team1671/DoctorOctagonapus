85ad560435f52f6063828cd180b47339305000f4
Buchanan Robotics <buchananrobotics@gmail.com>
2012-02-18 00:17:42 -0800

    Update drive/Vrisk.h

--- drive/Vrisk.h
+++ drive/Vrisk.h
@@ -3,33 +3,24 @@
 #define Kill GetWatchdog().Kill();
 
 #define kVOLTAGE 12.0
-#define RAMP .4
+#define RAMP 0
 #define MAX 2750
 #define KP 0.250//proportion for jags
 #define KI 0.015//integral change in proportion
 #define KD 0.020//damper
 #define kENCCOUNT 360
-	
+
+//#define dsio DriverStation::GetInstance()->GetEnhancedIO()
+
 class DoctaEight : public SimpleRobot
 {
 	Joystick pilot, copilot;
 	//	KinectStick *leftArm, *rightArm;
 	CANJaguar lefty, righty, leftyB, rightyB, intake, arm, LTop, LBot;
-	signed char distanceTarget, choiceTarget, negate, drive;
-	double firstTarget, secondTarget, thirdTarget;
-	bool limitedDistance, cycle, flag, aimin, shootin;
-	struct CamDataStruct
-	{
-		int numTargets;
-		unsigned int HP, LP;
-		double toCenter, 
-			centerY, 
-				centerX,
-					approx,
-						area, 
-							height, 
-								distanceYnorm;//stairway to heaven
-	}CamData;
+	signed char negate, drive;
+	bool limitedDistance, cycle, flag;
+	
+	
 public:
 ///////////////////////CORE
 		 DoctaEight(void);

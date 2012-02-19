04ef6538180089641b82374e8d8042f25988ec38
brotatos <9tumor@gmail.com>
2012-02-18 16:30:45 -0800

    Changes to be committed: 	deleted:    AutobotAnatomy 	deleted:    Computerip.bat 	deleted:    DoctorEightCamera.cpp 	deleted:    Dr.8/8Legs.cpp 	deleted:    Dr.8/Core.cpp 	deleted:    Dr.8/Sauron.cpp 	deleted:    Dr.8/Shaq.cpp 	deleted:    Dr.8/Vrisk.h 	deleted:    Official_Code/sys_ptc_win.jpg 	deleted:    Pointer_Version/Doc8_header.h 	deleted:    Pointer_Version/Doc8_pointers.cpp deleted:    Robotip.bat 	deleted:    TwiDash 	deleted:    diagdrive.cpp 	deleted:    drive/8Legs.cpp 	deleted:    drive/Core.cpp 	deleted:    drive/Vrisk.h

--- Computerip.bat
+++
@@ -1,9 +0,0 @@
-ECHO This fanciness is brought to you by Folders!
-ECHO You may have to change the text in "Wireless Network Connection"
-
-ECHO Resetting IP Address and Subnet Mask For DHCP
-netsh int ip set address name = "Wireless Network Connection" source = dhcp
-
-netsh int ip show config
-
-pause

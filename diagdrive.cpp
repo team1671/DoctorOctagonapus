9cad491fba0c8dc82e67ae6fa6acb940edbe41b0
brotatos <9tumor@gmail.com>
2012-02-18 16:38:20 -0800

    set up pointer branch

--- diagdrive.cpp
+++
@@ -1,9 +0,0 @@
-float x=pilot.GetTwist();
-float y=pilot.GetY();
-
-float rtot=1.41421356/2;//root(2)/2
-
-float forward=x*rtot+y*rtot;
-float MChammer=x*rtot-y*rtot//hammerslide- side to side
-
-//setmotors

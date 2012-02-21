f21d6b5513c5f962b8e220647086916897a74471
Eman96 <Iameman9669@gmail.com>
2012-02-21 11:42:10 +0800

    Rearranged and added functions changed 2/20/12 7:39

--- CANDrive.h
+++ CANDrive.h
@@ -11,33 +11,29 @@ private:
 				*m_canRightFront,
 				*m_canLeftBack,
 				*m_canRightBack;
-	
 	bool		m_bReversed;
-	
-	float		m_fLeftPosition, m_fRightPosition;
 	
 public:
 	CANDrive(void);
-	//CANDrive(const CANDrive &);
 	CANDrive(int _nLeftFront, int _nRightFront, int _nLeftBack, int _nRightBack);
-
+	//various functions
 	void SetPID(float _Kp, float _Ki, float _Kd);
-	
-	void SetValues(float _fLeftFront, float _fRightFront, float _fLeftBack, float _fRightBack);
 	void SetReversed(bool _bReverse);
+	void GetValues(float &_LF, float &_LB, float &_RF, float &_RB);
+	void ChangeControlMode(CANJaguar::ControlMode controlMode);
+	CANJaguar::ControlMode GetControlMode();
+	//valuecontrol
+	void SetValues(float _fLeftFront, float _fRightFront, float _fLeftBack, float _fRightBack);
 	void TankDrive(float _fLeftSpeed, float _fRightSpeed);
-	void DistDrive(float _fLeftDist, float _fRightDist);
+	void ArcadeDrive (float _fThrottleSpeed, float _fTurnSpeed);
+	bool DistDrive(float _fLeftDist, float _fRightDist);
+	void ThetaDrive(float angle);
+	//Usercontrol
+	void ArcadeDrive (Joystick *_stick);
 	void TankDrive(Joystick *_stick);
 	void TankDrive(KinectStick *_leftarm, KinectStick *_rightarm);
-	
-	void GetValues(float &_LF, float &_LB, float &_RF, float &_RB);
-	
-	void ArcadeDrive (float _fThrottleSpeed, float _fTurnSpeed);
-	void ArcadeDrive (Joystick *_stick);
-	
-	void SpeedTest (void);
+
+	void Test (void);
 };
-
-
 
 #endif

88d079b347563736c4b14fe6d94ad07a20f9f62d
Aaron <aramadan1671@gmail.com>
2012-03-08 16:39:15 -0800

    test as of 3-8-12 @ 4:39

--- Doc8.cpp
+++ Doc8.cpp
@@ -8,58 +8,59 @@
 //#include "skelton.h"
 
 class Doc8_Official : public SimpleRobot
-{	
+{
 	CANDrive *myCD;
 	Lift *myLift;
 	Shooter *myShooter;
-	
+
 	DriverStationLCD *driverOut;
-	
+
 	Joystick *pilot, *copilot;
 	KinectStick *leftArm, *rightArm;
 	Skeleton *rightLeg, leftLeg;
-	
-	
+	//test
+
+
 //	Joint *leftElbow, *rightElbow;
-	
+
 	int configuration;
 	bool bArcade, hasCamera;
-	
+
 	RampDescend *myRamp;
-	
+
 public:
 	Doc8_Official(void)
 	{
 		GetWatchdog().Kill();
 		//CONSTRUCTOR
-		myCD = new CANDrive(CAN_DRIVE_LEFT_FRONT, 							
+		myCD = new CANDrive(CAN_DRIVE_LEFT_FRONT,
 							CAN_DRIVE_RIGHT_FRONT,
 							CAN_DRIVE_LEFT_BACK,
 							CAN_DRIVE_RIGHT_BACK);
-		
+
 		myLift = new Lift (CAN_LIFT_TOWER);
-		
+
 		myShooter = new Shooter(CAN_SHOOTER_TOP,
 								CAN_SHOOTER_BOTTOM,
 								ENCOUNT_SHOOTER);
 		myShooter->SetPID(KP, KI, KD);
-		
+
 		myRamp = new RampDescend(CAN_RAMP_DESCENDER);
-		
+
 		driverOut = DriverStationLCD::GetInstance();
-		
+
 		pilot = new Joystick (JOYSTICK_PILOT_PORT);
 		copilot = new Joystick (JOYSTICK_COPILOT_PORT);
-		
-		
-		
+
+
+
 		leftArm = new KinectStick(KINECT_LEFT);
 		rightArm = new KinectStick(KINECT_RIGHT);
-		
+
 //		leftElbow = new Joint(ELBOW_RIGHT);
 //		rightElbow = new Joint(ELBOW_LEFT);
-		
-		
+
+
 		bArcade = false;
 		configuration = 1;//set to pee-sock l8er
 		hasCamera = false;//set to switch l8er
@@ -71,19 +72,19 @@ public:
 		GetWatchdog().Kill();
 		while(IsEnabled() && IsAutonomous())//this is kinda ghetto but..
 		{
-	
+
 			driverOut->Clear();
 			driverOut->PrintfLine(DriverStationLCD::kUser_Line1,"Autonomous");
 			myShooter->ChangeSpeed(SHOOTER_TOP_SPEED_MED,SHOOTER_BOTTOM_SPEED_MED);
-			
+
 			Wait(.3);
-			
-			
+
+
 			myShooter->m_canTopShooter->Set(SHOOTER_TOP_SPEED_CONFIG_ONE);
 			myShooter->m_canBottomShooter->Set(SHOOTER_BOTTOM_SPEED_CONFIG_ONE);
 			//myShooter->ChangeSpeed(SHOOTER_TOP_SPEED_MED,SHOOTER_BOTTOM_SPEED_MED);
 			//myShooter->UpdateValues();
-			
+
 			driverOut->PrintfLine(DriverStationLCD::kUser_Line2,"fire");
 			driverOut->UpdateLCD();
 
@@ -103,7 +104,7 @@ public:
 //					myShooter->ChangeSpeed(SHOOTER_TOP_SPEED_CONFIG_TWO,SHOOTER_BOTTOM_SPEED_CONFIG_TWO);
 //				}
 //			}
-			
+
 			Wait(2.0);
 			myLift->AutonomousLift(1);
 			Wait(4);
@@ -113,35 +114,35 @@ public:
 			/*
 			driverOut->PrintfLine(DriverStationLCD::kUser_Line3,"move to ramp");
 			driverOut->UpdateLCD();
-	
+
 			myCD->ArcadeDrive(-.7,0);
 			Wait(1.95);
 			myCD->ArcadeDrive(0,0);
 			//myCD->DistDrive(toRamp,toRamp);
-			
+
 			Wait(.1);
-			
+
 			driverOut->PrintfLine(DriverStationLCD::kUser_Line4,"descend");
 			driverOut->UpdateLCD();
 			myRamp->AutonomousArm(-1);
 			Wait(.5);
-			
-			
+
+
 			driverOut->PrintfLine(DriverStationLCD::kUser_Line5,"reset");
 			driverOut->UpdateLCD();
 			myRamp->AutonomousArm(1);
 			Wait(.4);
-			
+
 			myRamp->AutonomousArm(0);
-			
+
 			//Michael's
 			//myCD->Test();
 			*/
 			driverOut->Clear();
 			driverOut->PrintfLine(DriverStationLCD::kUser_Line4,"End of Autonomous: Beginning Kinect");
 			driverOut->UpdateLCD();
-			
-			
+
+
 			Kinect:
 				myCD->ArcadeDriveKinect(leftArm->GetY());
 				myRamp->AutonomousArm(rightArm->GetY());
@@ -150,7 +151,7 @@ public:
 				//kinect support
 			if (IsAutonomous() && IsEnabled())
 				goto Kinect;
-			
+
 			driverOut->Clear();
 			driverOut->PrintfLine(DriverStationLCD::kUser_Line4,"End of Hybrid");
 			driverOut->UpdateLCD();
@@ -161,7 +162,7 @@ public:
 	{
 		GetWatchdog().Kill();
 		while(IsEnabled() && IsOperatorControl())
-		{			
+		{
 			//SHOOTING SPEEDS BY BUTTON SELECTION
 			if (!hasCamera or copilot->GetRawButton(9))//!
 			{
@@ -188,9 +189,9 @@ public:
 			}
 			else
 			{
-				
+
 			}
-			
+
 			//SWITCHING TO AND FROM TANK AND ARCADE
 			if (pilot->GetRawButton(6))
 			{
@@ -200,16 +201,16 @@ public:
 			{
 				bArcade = false;
 			}
-			
+
 			if (bArcade)
 			{
 				myCD->ArcadeDrive(pilot);
 			}
-			else 
-			{	
+			else
+			{
 				myCD -> TankDrive(pilot);
 			}
-			
+
 			//REVERSED TANK DRIVE AND ARCADE DRIVE (MUST HAVE SELECTION WITH G.R.B. 5 & 6
 			if (pilot->GetRawButton(1))
 			{
@@ -219,16 +220,16 @@ public:
 			{
 				myCD -> SetReversed(false);
 			}
-			
+
 			myLift->Control(copilot);
 			myShooter->Control(copilot);
 			myShooter->UpdateValues();
 			myRamp->Control(pilot);
-			
+
 			float lf, lb, rf, rb;
-			
+
 			myCD->GetValues(lf, lb, rf, rb);
-			
+
 			driverOut->Clear();
 			driverOut->PrintfLine(DriverStationLCD::kUser_Line1,"TopShotRPM:%.4f", myShooter->GetTopSpeed());
 			driverOut->PrintfLine(DriverStationLCD::kUser_Line2,"BotShotRPM:%.4f", myShooter->GetBottomSpeed());
@@ -237,7 +238,7 @@ public:
 			driverOut->PrintfLine(DriverStationLCD::kUser_Line5,"Front, %f, %f", lf, rf);
 			driverOut->PrintfLine(DriverStationLCD::kUser_Line6,"Back, %f, %f", lb, rb);
 			driverOut->UpdateLCD();
-			
+
 		}
 	}
 };
